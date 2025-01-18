3d42e5469df2a3a5391baa6d0a4ad2a0cac04bde
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2025-01-18 07:35:11 +0100

    dashboard sidebar

--- take-care-mind-frontend/src/components/Sidebar.tsx
+++ take-care-mind-frontend/src/components/Sidebar.tsx
@@ -1,51 +1,126 @@
+import React, { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { FiHome, FiUser, FiSettings, FiLogOut } from "react-icons/fi";
+import {
+  FiSettings,
+  FiChevronDown,
+  FiChevronRight,
+  FiHome,
+  FiBox,
+  FiLayers,
+  FiPieChart,
+  FiStar,
+  FiUsers,
+} from "react-icons/fi";
+import { usePathname } from "next/navigation";
 
-const Sidebar = () => {
-  const router = useRouter();
+interface MenuItem {
+  icon: React.ElementType;
+  text: string;
+  href?: string;
+  subItems?: MenuItem[];
+}
 
-  const menuItems = [
-    { icon: FiHome, text: "Accueil", href: "/dashboard" },
-    { icon: FiUser, text: "Profil", href: "/dashboard/profile" },
-    { icon: FiSettings, text: "Paramètres", href: "/dashboard/settings" },
+const Sidebar: React.FC = () => {
+  const [openAccordions, setOpenAccordions] = useState<number[]>([1]);
+  const pathname = usePathname();
+
+  const menuItems: MenuItem[] = [
+    { icon: FiHome, text: "Dashboard", href: "/dashboard" },
+    {
+      icon: FiBox,
+      text: "Fonctionnalités",
+      subItems: [
+        { icon: FiLayers, text: "Méditation", href: "/features/function1" },
+        { icon: FiLayers, text: "Objectifs", href: "/features/function2" },
+        { icon: FiLayers, text: "Activité", href: "/features/function3" },
+        { icon: FiLayers, text: "Routine", href: "/features/function3" },
+        { icon: FiLayers, text: "Stoisisme", href: "/features/function3" },
+        { icon: FiLayers, text: "Motivation", href: "/features/function3" },
+      ],
+    },
+    { icon: FiPieChart, text: "Analyse", href: "/analysis" },
+    { icon: FiSettings, text: "Paramètres", href: "/settings" },
+    { icon: FiStar, text: "Premium", href: "/premium" },
+    { icon: FiUsers, text: "Communauté", href: "/community" },
   ];
 
-  const handleLogout = () => {
-    // Ajoutez ici votre logique de déconnexion
-    router.push("/auth/login");
+  const toggleAccordion = (index: number) => {
+    setOpenAccordions((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+    );
+  };
+
+  const isActive = (href: string) => pathname === href;
+
+  const renderMenuItem = (item: MenuItem, index: number) => {
+    if (item.subItems) {
+      const isOpen = openAccordions.includes(index);
+      return (
+        <li key={index}>
+          <button
+            onClick={() => toggleAccordion(index)}
+            className="flex items-center justify-between w-full px-6 py-3 text-sm text-gray-300 hover:bg-gray-700 hover:text-white transition-colors duration-200"
+          >
+            <span className="flex items-center">
+              <item.icon className="mr-3" />
+              {item.text}
+            </span>
+            {isOpen ? <FiChevronDown /> : <FiChevronRight />}
+          </button>
+          {isOpen && (
+            <ul className="ml-6 mt-2">
+              {item.subItems.map((subItem, subIndex) => (
+                <li key={subIndex}>
+                  <Link
+                    href={subItem.href || "#"}
+                    className={`flex items-center px-6 py-2 text-sm text-gray-400 hover:bg-gray-700 hover:text-white transition-colors duration-200 ${
+                      isActive(subItem.href || "")
+                        ? "bg-gray-700 text-white"
+                        : "text-gray-400 hover:bg-gray-700 hover:text-white"
+                    }`}
+                  >
+                    {/* <subItem.icon className="mr-3" /> */}
+                    {subItem.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      );
+    }
+
+    return (
+      <li key={index}>
+        <Link
+          href={item.href || "#"}
+          className={`flex items-center px-6 py-3 text-sm text-gray-300 hover:bg-gray-700 hover:text-white transition-colors duration-200 ${
+            isActive(item.href || "")
+              ? "bg-gray-700 text-white"
+              : "text-gray-300 hover:bg-gray-700 hover:text-white"
+          } transition-colors duration-200`}
+        >
+          <item.icon className="mr-3" />
+          {item.text}
+        </Link>
+      </li>
+    );
   };
 
   return (
-    <aside className="w-64 bg-gray-800 text-white relative">
-      <div className="p-6">
-        <h2 className="text-2xl font-semibold">Mon App</h2>
+    <div className="bg-gray-800 text-white h-full">
+      <div className="px-6 py-4 my-4">
+        <Link href="/" className="text-2xl font-bold">
+          Take Care Mind
+        </Link>
       </div>
       <nav className="mt-6">
-        <ul>
-          {menuItems.map((item, index) => (
-            <li key={index}>
-              <Link
-                href={item.href}
-                className="flex items-center px-6 py-3 text-gray-300 hover:bg-gray-700 hover:text-white transition-colors duration-200"
-              >
-                <item.icon className="mr-3" />
-                {item.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul>{menuItems.map((item, index) => renderMenuItem(item, index))}</ul>
       </nav>
-      <div className="absolute bottom-0 w-full">
-        <button
-          onClick={handleLogout}
-          className="flex items-center w-full px-6 py-3 text-gray-300 hover:bg-gray-700 hover:text-white transition-colors duration-200"
-        >
-          <FiLogOut className="mr-3" />
-          Déconnexion
-        </button>
+      <div>
+        
       </div>
-    </aside>
+    </div>
   );
 };
 
